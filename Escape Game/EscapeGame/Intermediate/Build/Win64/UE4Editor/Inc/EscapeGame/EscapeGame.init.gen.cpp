86ff2dac64ea3a6aea00f08a63121bebb8f8979c
Sarah Cheung <cheungsa@usc.edu>
2018-03-05 17:38:39 -0800

    Starting point of creating game

---
+++ Escape Game/EscapeGame/Intermediate/Build/Win64/UE4Editor/Inc/EscapeGame/EscapeGame.init.gen.cpp
@@ -0,0 +1,34 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "GeneratedCppIncludes.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeEscapeGame_init() {}
+	UPackage* Z_Construct_UPackage__Script_EscapeGame()
+	{
+		static UPackage* ReturnPackage = nullptr;
+		if (!ReturnPackage)
+		{
+			static const UE4CodeGen_Private::FPackageParams PackageParams = {
+				"/Script/EscapeGame",
+				PKG_CompiledIn | 0x00000000,
+				0xB893136B,
+				0x2F29C7F3,
+				nullptr, 0,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
+		}
+		return ReturnPackage;
+	}
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
---
+++ Escape Game/EscapeGame/Intermediate/Build/Win64/UE4Editor/Inc/EscapeGame/EscapeGameGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ESCAPEGAME_EscapeGameGameModeBase_generated_h
+#error "EscapeGameGameModeBase.generated.h already included, missing '#pragma once' in EscapeGameGameModeBase.h"
+#endif
+#define ESCAPEGAME_EscapeGameGameModeBase_generated_h
+
+#define EscapeGame_Source_EscapeGame_EscapeGameGameModeBase_h_15_RPC_WRAPPERS
+#define EscapeGame_Source_EscapeGame_EscapeGameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define EscapeGame_Source_EscapeGame_EscapeGameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAEscapeGameGameModeBase(); \
+	friend ESCAPEGAME_API class UClass* Z_Construct_UClass_AEscapeGameGameModeBase(); \
+public: \
+	DECLARE_CLASS(AEscapeGameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/EscapeGame"), NO_API) \
+	DECLARE_SERIALIZER(AEscapeGameGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define EscapeGame_Source_EscapeGame_EscapeGameGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAEscapeGameGameModeBase(); \
+	friend ESCAPEGAME_API class UClass* Z_Construct_UClass_AEscapeGameGameModeBase(); \
+public: \
+	DECLARE_CLASS(AEscapeGameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/EscapeGame"), NO_API) \
+	DECLARE_SERIALIZER(AEscapeGameGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define EscapeGame_Source_EscapeGame_EscapeGameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AEscapeGameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AEscapeGameGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AEscapeGameGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AEscapeGameGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AEscapeGameGameModeBase(AEscapeGameGameModeBase&&); \
+	NO_API AEscapeGameGameModeBase(const AEscapeGameGameModeBase&); \
+public:
+
+
+#define EscapeGame_Source_EscapeGame_EscapeGameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AEscapeGameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AEscapeGameGameModeBase(AEscapeGameGameModeBase&&); \
+	NO_API AEscapeGameGameModeBase(const AEscapeGameGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AEscapeGameGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AEscapeGameGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AEscapeGameGameModeBase)
+
+
+#define EscapeGame_Source_EscapeGame_EscapeGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define EscapeGame_Source_EscapeGame_EscapeGameGameModeBase_h_12_PROLOG
+#define EscapeGame_Source_EscapeGame_EscapeGameGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	EscapeGame_Source_EscapeGame_EscapeGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	EscapeGame_Source_EscapeGame_EscapeGameGameModeBase_h_15_RPC_WRAPPERS \
+	EscapeGame_Source_EscapeGame_EscapeGameGameModeBase_h_15_INCLASS \
+	EscapeGame_Source_EscapeGame_EscapeGameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define EscapeGame_Source_EscapeGame_EscapeGameGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	EscapeGame_Source_EscapeGame_EscapeGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	EscapeGame_Source_EscapeGame_EscapeGameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	EscapeGame_Source_EscapeGame_EscapeGameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	EscapeGame_Source_EscapeGame_EscapeGameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID EscapeGame_Source_EscapeGame_EscapeGameGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
